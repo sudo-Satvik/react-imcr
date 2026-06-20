5604359c3edd2ea82027eed4b736503bce84d00e
Satvik Sharma <sharmasatvik43@gmail.com>
2026-06-20 14:28:51 +0530

    refactor: streamline state management in Accordion component for improved clarity and functionality

--- src/components/ui/accordian/index.tsx
+++ src/components/ui/accordian/index.tsx
@@ -3,31 +3,31 @@ import Wrapper from "../../other/Wrapper";
 import ACCORDION_DATA from "./data";
 
 export default function Accordion() {
-  const [selected, setSelected] = useState<string | null>(null);
-  const [isMultiSelect, setIsMultiSelect] = useState<boolean>(false);
-  const [multiple, setMultiple] = useState<string[]>([]);
+  const [isMultiple, setIsMultiple] = useState<boolean>(false);
+  const [singleSelect, setSingleSelect] = useState<string | null>(null);
+  const [multipleSelect, setMultipleSelect] = useState<string[]>([]);
 
-  const handleSingleSelection = (selectedId: string): void => {
-    setSelected(selectedId === selected ? null : selectedId);
-  };
+  const handleSingleSelection = (selectedId: string): void =>
+    setSingleSelect(selectedId === singleSelect ? null : selectedId);
 
   const handleMultipleSelection = (selectedId: string): void => {
-    const clone = [...multiple];
-    const selectedIdIndex = clone.indexOf(selectedId);
+    let cloneArray = [...multipleSelect];
 
-    if (selectedIdIndex === -1) {
-      clone.push(selectedId);
+    const selectedIndex = cloneArray.indexOf(selectedId);
+
+    if (selectedIndex === -1) {
+      cloneArray.push(selectedId);
     } else {
-      clone.splice(selectedIdIndex, 1);
+      cloneArray.splice(selectedIndex, 1);
     }
 
-    setMultiple(clone);
+    setMultipleSelect(cloneArray);
   };
 
-  const toggleMode = (): void => {
-    setIsMultiSelect((prev) => !prev);
-    setSelected(null);
-    setMultiple([]);
+  const toggleMultiple = () => {
+    setIsMultiple((prev) => !prev);
+    setSingleSelect(null);
+    setMultipleSelect([]);
   };
 
   return (
@@ -37,34 +37,35 @@ export default function Accordion() {
         <div className="flex gap-3 items-center mb-4">
           <div
             className={`w-10 h-5 rounded-full cursor-pointer transition-all ${
-              isMultiSelect ? "bg-blue-300" : "bg-gray-300"
+              isMultiple ? "bg-blue-300" : "bg-gray-300"
             }`}
-            onClick={toggleMode}
+            onClick={toggleMultiple}
           >
             <div
               className={`w-5 h-5 bg-blue-600 rounded-full shadow-lg transition-all ${
-                isMultiSelect ? "translate-x-5" : "translate-x-0"
+                isMultiple ? "translate-x-5" : "translate-x-0"
               }`}
             />
           </div>
 
-          <p className="text-md font-semibold">Toggle Multi-Select Accordion</p>
+          <p className="text-md font-semibold">
+            Toggle Multi-Select Accordion {isMultiple ? "Off" : "On"}
+          </p>
         </div>
 
         {ACCORDION_DATA?.length > 0 ? (
           ACCORDION_DATA.map((item) => {
-            const isOpen = isMultiSelect
-              ? multiple.includes(item.id)
-              : selected === item.id;
-
+            const isOpen = isMultiple
+              ? multipleSelect.includes(item.id)
+              : singleSelect === item.id;
             return (
               <div
                 key={item.id}
                 className="max-w-130 cursor-pointer px-3 py-2 rounded-lg bg-slate-300 select-none"
-                onClick={
-                  isMultiSelect
-                    ? () => handleMultipleSelection(item.id)
-                    : () => handleSingleSelection(item.id)
+                onClick={() =>
+                  !isMultiple
+                    ? handleSingleSelection(item.id)
+                    : handleMultipleSelection(item.id)
                 }
               >
                 {/* Title */}
@@ -76,11 +77,9 @@ export default function Accordion() {
 
                 {/* Content */}
                 <div
-                  className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                    isOpen
-                      ? "max-h-40 opacity-100 py-3"
-                      : "max-h-0 opacity-0 py-0"
-                  }`}
+                  className={`overflow-hidden transition-all duration-300 ease-in-out
+                    ${isOpen ? "max-h-40 opacity-100" : "max-h-0 opacity-0"}
+                    `}
                 >
                   <div className="px-2 py-3">{item.answer}</div>
                 </div>
