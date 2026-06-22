fc20253ec52fc26534c6619eaae10c579ed8167d
Satvik Sharma <sharmasatvik43@gmail.com>
2026-06-22 23:44:24 +0530

    feat: enhance LoadMore component with error handling and loading state management

--- src/components/ui/load-more/index.tsx
+++ src/components/ui/load-more/index.tsx
@@ -18,6 +18,7 @@ const LoadMore = () => {
   const [error, setError] = useState<string | null>(null);
   const [productData, setProductData] = useState<Product[]>([]);
   const [skipCount, setSkipCount] = useState(0);
+  const [disableLoadMoreBtn, setDisableLoadMoreBtn] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -27,16 +28,23 @@ const LoadMore = () => {
           `https://dummyjson.com/products?limit=${LIMIT}&skip=${skipCount === 0 ? 0 : skipCount * LIMIT}`,
         );
 
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
         const data = await response.json();
 
-        if (data && data?.products && data?.products.length) {
+        if (data?.products) {
           setProductData((prev) => [...prev, ...data.products]);
-          setLoading(false);
           setError(null);
         }
       } catch (e) {
-        console.error(e?.message);
-        setError(e?.message);
+        if (e instanceof Error) {
+          setError(e.message);
+        } else {
+          setError("Something went wrong");
+        }
+      } finally {
         setLoading(false);
       }
     };
@@ -44,14 +52,23 @@ const LoadMore = () => {
     fetchProducts();
   }, [skipCount]);
 
+  useEffect(() => {
+    if (productData && productData.length >= 100) {
+      setDisableLoadMoreBtn(true);
+    }
+  }, [productData]);
+
   const truncateText = (text: string, maxLength: number): string => {
     return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
   };
 
-  if (loading)
-    <Wrapper bgColor="bg-indigo-200" isFlex>
-      <p className="text-5xl font-semibold mt-10">Loading Module...</p>
-    </Wrapper>;
+  if (loading) {
+    return (
+      <Wrapper bgColor="bg-indigo-200" isFlex>
+        <p className="text-5xl font-semibold mt-10">Loading Module...</p>
+      </Wrapper>
+    );
+  }
 
   return (
     <Wrapper bgColor="bg-indigo-200" isFlex>
@@ -73,11 +90,18 @@ const LoadMore = () => {
         )}
       </div>
       <button
-        className="rounded-2xl py-3 px-6 bg-white/70 border border-white/20 active:scale-95 transition-all font-semibold cursor-pointer -mt-10 mb-10"
+        className={`rounded-2xl py-3 px-6 bg-white/70 border border-white/20 active:scale-95 transition-all font-semibold cursor-pointer -mt-10 mb-10 ${disableLoadMoreBtn ? "opacity-50" : "opacity-100"}`}
         onClick={() => setSkipCount((prev) => prev + 1)}
+        disabled={disableLoadMoreBtn || loading}
       >
         Load More
       </button>
+      {disableLoadMoreBtn && (
+        <p className="text-md mb-10 -mt-10">
+          You have reached to end of the page. Total Products{" "}
+          {productData.length}
+        </p>
+      )}
     </Wrapper>
   );
 };
