f741f3ab5d5dc3c610982fe50d2e9b7fe10233ec
Satvik Sharma <sharmasatvik43@gmail.com>
2026-06-24 19:37:43 +0530

    feat: implement QRCodeGenerator component for generating QR codes

--- src/components/ui/qr-code-generator/index.tsx
+++ src/components/ui/qr-code-generator/index.tsx
@@ -1,20 +1,39 @@
+import QRCode from "react-qr-code";
 import Wrapper from "../../other/Wrapper";
+import { useState } from "react";
 
 const QRCodeGenerator = () => {
+  const [qrCodeValue, setQrCodeValue] = useState("");
+
   return (
     <Wrapper heading="QR Code Generator" bgColor="bg-amber-100">
-      <div className="w-100 h-100 bg-gray-600"></div>
+      <div className="flex flex-col items-center gap-6">
+        <div className="bg-gray-600 p-5 rounded-2xl shadow-lg">
+          <QRCode
+            size={300}
+            bgColor="#4a5565"
+            fgColor="#ffffff"
+            value={qrCodeValue || " "}
+          />
+        </div>
 
-      <div className="flex items-center gap-2">
         <input
           type="text"
-          placeholder="Enter Your Value Here..."
-          className="flex-1 px-4 py-2 border border-gray-400 rounded-lg outline-none focus:ring-0 focus:ring-gray-500 focus:border-gray-500 transition"
+          value={qrCodeValue}
+          onChange={(e) => setQrCodeValue(e.target.value)}
+          placeholder="Enter text or URL..."
+          className="
+            w-80
+            px-4
+            py-3
+            border
+            border-gray-300
+            rounded-lg
+            outline-none
+            focus:ring-2
+            focus:ring-blue-500
+          "
         />
-
-        <button className="px-4 py-2 bg-gray-600 text-white rounded-lg hover:bg-gray-700 active:scale-95 transition">
-          Search
-        </button>
       </div>
     </Wrapper>
   );
